1e19ce715e3265a6e45a68807d1147f8e918b58d
Edilson Júnior <edilsonleite@softeam.com.br>
2019-12-19 13:09:05 -0300

    delete by id

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -14,8 +14,8 @@ router.get('/', (req, res, next) => {
 });
 
 router.get('/planetas', (req, res) => {
-    Planeta.find().sort({nome_planeta: 'asc'}).then((planetas) => {
-        res.send({planetas});
+    Planeta.find().sort({ nome_planeta: 'asc' }).then((planetas) => {
+        res.send({ planetas });
     }).catch((err) => {
         res.send(err);
     })
@@ -24,15 +24,15 @@ router.get('/planetas', (req, res) => {
 router.post('/planetas', (req, res) => {
     var erros = [];
 
-    if(!req.body.nome_planeta || 
-        req.body.nome_planeta == undefined || 
-        req.body.nome_planeta == null){
-            erros.push({texto: "Nome inválido"})
+    if (!req.body.nome_planeta ||
+        req.body.nome_planeta == undefined ||
+        req.body.nome_planeta == null) {
+        erros.push({ texto: "Nome inválido" })
     }
 
-    if(erros.length > 0){
-        res.render("admin/addplaneta", {erros: erros})
-    }else{
+    if (erros.length > 0) {
+        res.render("admin/addplaneta", { erros: erros })
+    } else {
         const novoPlaneta = {
             nome_planeta: req.body.nome_planeta,
             tam_planeta: req.body.tam_planeta,
@@ -40,27 +40,28 @@ router.post('/planetas', (req, res) => {
             gravidade_planeta: req.body.gravidade_planeta,
             comp_planeta: req.body.comp_planeta
         };
-    
+
         new Planeta(novoPlaneta).save().then(() => {
             res.status(200).send('Requisição recebida com sucesso!');
         }).catch((err) => {
             res.status(404).send('Deu ruim');
-        });    
+        });
     }
 });
 
-router.delete("/planetas/del", (req, res) => {
-    Planetas.remove({_id: req.body.id}).then(() => {
+router.delete("/planetas/del/:id", (req, res) => {
+    Planetas.findOne({ _id: req.params.id }).then(() => {
         res.status(200).send('Deletou de boas')
     }).catch((err) => {
         res.status(404).send('Deu ruim');
-    });
+    })
+
 });
 
 router.get("/satelites", (req, res) => {
 
-    Satelite.find().populate("planeta").sort({data: "asc"}).then((satelites) => {
-        res.send({satelites})
+    Satelite.find().populate("planeta").sort({ data: "asc" }).then((satelites) => {
+        res.send({ satelites })
     }).catch((err) => {
         req.flash("error_msg", "Houve um erro ao listar os satélites");
         res.redirect("/admin");
