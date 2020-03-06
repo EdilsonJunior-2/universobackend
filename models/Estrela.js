84f4c8fbe15f72dab7b2f9563e62c6724cd1c836
Edilson <edilsonleite@softeam.com.br>
2020-03-06 15:31:06 -0300

    schema de estrelas, get de estrelas e anãs brancas

---
+++ models/Estrela.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+var Float = require('mongoose-float').loadType(mongoose);
+
+const Estrela = new Schema({
+    nome_estrela: {
+        type: String,
+        required: true
+    },
+    idade_estrela: Number,
+    distancia_tela: Number,
+    gravidade_estrela: Float,
+    tipo_estrela: {
+        type: String,
+        default: ""
+    }
+});
+
+mongoose.model("estrelas", Estrela);
